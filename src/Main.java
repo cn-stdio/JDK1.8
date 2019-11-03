4bbbb52bb94305497663b947446346fdcf15bdb0
Seaguller <cn_stdio@163.com>
2019-11-03 17:31:53 +0800

    项目初始化

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Seagull_gby
+ * @date 2019/10/22 14:39
+ * Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        List l = new ArrayList();
+        l.add("111");
+        System.out.println("abc" + l);
+    }
+}
